04549407b8d78e1173d38b3c174e3ab4b7afd336
RobertStoyanov0210 <48961580+RobertStoyanov0210@users.noreply.github.com>
2019-04-01 18:43:04 +0300

    Create Subset.h

---
+++ Subset.h
@@ -0,0 +1,15 @@
+#ifndef _SUBSET_H
+#define _SUBSET_H
+
+int fillArray(int, int, int **);
+int PrintArray(int, int, int **);
+bool Injection(int, int **);
+bool Surjection(int, int **);
+bool Permutation(int, int **);
+int PermutationsCounter(int, int **);
+bool FixedPoints(int, int **);
+int Identity(int, int**);
+void IndependentCycles(int, int **, int *);
+bool AllDigitsAreTheSame(int**, int);
+
+#endif
