b901204705e00750b4f492857b99db2b18025252
RobertStoyanov0210 <48961580+RobertStoyanov0210@users.noreply.github.com>
2019-04-01 18:44:05 +0300

    Create HeaderEx12.h

---
+++ HeaderEx12.h
@@ -0,0 +1,10 @@
+#ifndef _HEADEREX12_H
+#define _HEADEREX12_H
+
+void IndepCycleToMatrix(char*);
+int GetCurrentNumber(char*, int, int);
+bool InputIsValid(int**, int);
+void FillMatrix(char*, int);
+void PrintMatrix(int**, int);
+
+#endif
